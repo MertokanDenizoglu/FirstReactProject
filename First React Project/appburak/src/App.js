62891e64602772ab80e0485aaa9e43533149cf83
Mert Okan <106598952+MertokanDenizoglu@users.noreply.github.com>
2022-06-12 03:05:39 +0300

    FirstReactProject
    FirstReactProject

---
+++ First React Project/appburak/src/App.js
@@ -0,0 +1,16 @@
+import Nav from './nav/Nav';
+import './App.css';
+import Navbar from './navbar/navbar'
+import Slider from './slider/slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Navbar/>
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
